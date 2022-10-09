698851b42be56d9642ac76971b16c0812cb7682e
zmsen <115074106+zmsen@users.noreply.github.com>
2022-10-09 14:50:54 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -6,7 +6,7 @@ export default function Header({ name }) {
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl dark:text-white text-center">
-        <Link href="/">
+        <Link href="https://t.me/+fEb6_z9Rh0QxNzQy">
           <a>{name}</a>
         </Link>
       </p>
