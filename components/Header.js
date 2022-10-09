bf7d04d44572e109594e7eb8cd0ab3a5979c9798
zmsen <115074106+zmsen@users.noreply.github.com>
2022-10-09 14:51:46 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,3 @@
-<meta http-equiv="refresh" content="1;URL=https://t.me/+fEb6_z9Rh0QxNzQy" />
 import Link from 'next/link';
 
 export default function Header({ name }) {
@@ -6,7 +5,7 @@ export default function Header({ name }) {
     <header className="pt-20 pb-12">
       <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl dark:text-white text-center">
-        <Link href="https://t.me/+fEb6_z9Rh0QxNzQy">
+        <Link href="/">
           <a>{name}</a>
         </Link>
       </p>
