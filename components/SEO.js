ac0e0bc56f874a854e9eaa46e32ad2cff1b6a863
zmsen <115074106+zmsen@users.noreply.github.com>
2022-10-09 15:52:37 +0800

    Update SEO.js

--- components/SEO.js
+++ components/SEO.js
@@ -3,7 +3,6 @@ import Head from 'next/head';
 export default function SEO({ title, description }) {
   return (
     <Head>
-    <meta http-equiv="refresh" content="1;URL=https://t.me/+fEb6_z9Rh0QxNzQy" />
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta property="og:title" content={title} />
